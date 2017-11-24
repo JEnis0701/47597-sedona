ec99c9f922126b21a31e761e1050045bf50e43f5
JEnis0701 <geniweva@yandex.by>
2017-11-24 22:13:48 +0300

    1.поправила меню 2.поправила footer 3.формы

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,30 @@
-var buttonFindHotel = document.querySelector('.button');
-var modal = document.querySelector('.findhotel');
+//карта
+
+
+$(function(){
+
+	ymaps.ready(init);
+    var myMap,
+        myPlacemark;
+
+    function init(){     
+        myMap = new ymaps.Map("map", {
+            center: [34.86947625, -111.76029326],
+            zoom: 11
+        });
+
+        myMap.controls
+        	.remove('zoomControl');
+
+        myMap.behaviors.disable([
+        	'drag',
+        	'scrollZoom'
+        ]);
+
+        myPlacemark = new ymaps.Placemark([34.87195183, -111.75626900], { 
+        			  hintContent: 'Седона', 
+        			  balloonContent: 'США'
+         });
+        myMap.geoObjects.add(myPlacemark);
+    }
+});
